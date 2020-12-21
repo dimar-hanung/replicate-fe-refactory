e5e4618fb8486d8029e9d2d526d8ba6f620191ee
dimarhanung <dimarhanung@ecampus.ut.ac.id>
2020-12-21 19:36:37 +0700

    chore(add): init package install

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import "@/assets/styles/index.scss";
+import "@/assets/styles/package/nprogress.css";
+
+// start - Vue Toast Message
+import VueToast from "vue-toast-notification";
+import "@/assets/styles/package/theme-sugar.css";
+Vue.use(VueToast, {
+  position: "top-right",
+  duration: 2500
+});
+// end - Vue Toast Message
 
 Vue.config.productionTip = false;
 
